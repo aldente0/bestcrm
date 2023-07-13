f9bced39cbac1b26555a108247628fa365ea7ece
Алексей Ложкин <leha.lozhkin.02@mail.ru>
2023-07-13 15:19:36 +0300

    change logo in navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+                        <h2 class="display-block ml-4 text-xl font-semibold text-gray-900 dark:text-white">bestCRM</h2>
                     </a>
                 </div>
 
