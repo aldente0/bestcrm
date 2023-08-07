4de69a5dd648f9e0106ad61246c835fb52116e86
Алексей Ложкин <leha.lozhkin.02@mail.ru>
2023-08-07 09:30:32 +0300

    change orders' table view

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -18,28 +18,37 @@
         </div> --}}
     </x-slot>
     {{-- TODO add simply table to show orders --}}
-    <table class=" bg-white mx-auto">
-        <thead>
-            <tr>
-                <th class="w-48 border border-slate-300 ...">Наименование</th>
-                <th class="w-48 border border-slate-300 ...">Цена в руб.</th>
-                <th class="w-48 border border-slate-300 ...">Количество</th>
-                <th class="w-48 border border-slate-300 ...">Стоимость в руб.</th>
-                <th class="w-48 border border-slate-300 ...">Дата создания</th>
-            </tr>
-        </thead>
-        <tbody>
-            @isset($orders)
-                @foreach ($orders as $order)
-                    <tr>
-                        <td class="pl-2 border border-slate-300 ...">{{$order->product}}</td>
-                        <td class="pl-2 border border-slate-300 ...">{{$order->price / 100}}</td>
-                        <td class="pl-2 border border-slate-300 ...">{{$order->quantity}}</td>
-                        <td class="pl-2 border border-slate-300 ...">{{$order->price * $order->quantity / 100}}</td>
-                        <td class="pl-2  pr-5 border border-slate-300 ...">{{$order->created_at}}</td>
+    <div class="flex flex-col w-10/12 mx-auto">
+        <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+            <div class="overflow-hidden">
+              <table class="min-w-full text-left text-sm font-light">
+                <thead class="border-b font-medium dark:border-neutral-500">
+                  <tr>
+                    <th scope="col" class="px-6 py-4">Наименование</th>
+                    <th scope="col" class="px-6 py-4">Цена в руб.</th>
+                    <th scope="col" class="px-6 py-4">Количество</th>
+                    <th scope="col" class="px-6 py-4">Стоимость</th>
+                    <th scope="col" class="px-6 py-4">Дата создания</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  @isset($orders)
+                    @foreach ($orders as $order)
+                    <tr class="border-b dark:border-neutral-500">
+                      <td class="whitespace-nowrap px-6 py-4">{{$order->product}}</td>
+                      <td class="whitespace-nowrap px-6 py-4">{{$order->price / 100}}</td>
+                      <td class="whitespace-nowrap px-6 py-4">{{$order->quantity}}</td>
+                      <td class="whitespace-nowrap px-6 py-4">{{$order->price * $order->quantity / 100}}</td>
+                      <td class="whitespace-nowrap px-6 py-4">{{$order->created_at}}</td>
                     </tr>
-                @endforeach
-            @endisset
-        </tbody>
-      </table>
+                    @endforeach
+                  @endisset
+
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
 </x-app-layout>
