7a5d42d7eff77f4d5b446266aefb41c3c8c88dfb
Алексей Ложкин <leha.lozhkin.02@mail.ru>
2023-08-07 13:58:45 +0300

    add attribute color to text component

--- app/View/Components/Text.php
+++ app/View/Components/Text.php
@@ -11,7 +11,9 @@ class Text extends Component
     /**
      * Create a new component instance.
      */
-    public function __construct()
+    public function __construct(
+        public string $color,
+    )
     {
         //
     }
